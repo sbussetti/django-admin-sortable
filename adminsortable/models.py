11188f1f19c0084e5283d934f47118ed4b261394
sbussetti <sbussetti@freelancersunion.org>
2013-05-01 14:57:23 -0400

    original github is out of date? copied over most recent version from pypi and then added some logging

--- adminsortable/models.py
+++ adminsortable/models.py
@@ -14,22 +14,23 @@ class MultipleSortableForeignKeyException(Exception):
 
 class Sortable(models.Model):
     """
-    Unfortunately, Django doesn't support using more than one AutoField in a model
-    or this class could be simplified.
+    Unfortunately, Django doesn't support using more than one AutoField
+    in a model or this class could be simplified.
 
-    `is_sortable` determines whether or not the Model is sortable by determining
-    if the last value of `order` is greater than the default of 1, which should be
-    present if there is only one object.
+    `is_sortable` determines whether or not the Model is sortable by
+    determining if the last value of `order` is greater than the default
+    of 1, which should be present if there is only one object.
 
-    `model_type_id` returns the ContentType.id for the Model that inherits Sortable
+    `model_type_id` returns the ContentType.id for the Model that
+    inherits Sortable
 
-    `save` the override of save increments the last/highest value of order by 1
-
-    Override `sortable_by` method to make your model be sortable by a foreign key field.
-    Set `sortable_by` to the class specified in the foreign key relationship.
+    `save` the override of save increments the last/highest value of
+    order by 1
     """
 
-    order = models.PositiveIntegerField(editable=False, default=1, db_index=True)
+    order = models.PositiveIntegerField(editable=False, default=1,
+        db_index=True)
+    is_sortable = False
 
     # legacy support
     sortable_by = None
@@ -38,13 +39,18 @@ class Sortable(models.Model):
         abstract = True
         ordering = ['order']
 
-    @classmethod
-    def is_sortable(cls):
-        try:
-            max_order = cls.objects.aggregate(models.Max('order'))['order__max']
-        except (TypeError, IndexError):
-            max_order = 0
-        return True if max_order > 1 else False
+    # @classmethod
+    # def determine_if_sortable(cls):
+    #     try:
+    #         max_order = cls.objects.aggregate(
+    #             models.Max('order'))['order__max']
+    #     except (TypeError, IndexError):
+    #         max_order = 0
+
+    #     if max_order > 1:
+    #         cls.is_sortable = True
+    #     else:
+    #         cls.is_sortable = False
 
     @classmethod
     def model_type_id(cls):
@@ -59,12 +65,14 @@ class Sortable(models.Model):
             if isinstance(field, SortableForeignKey):
                 sortable_foreign_keys.append(field)
         if len(sortable_foreign_keys) > 1:
-            raise MultipleSortableForeignKeyException(u'%s may only have one SortableForeignKey' % self)
+            raise MultipleSortableForeignKeyException(
+                u'%s may only have one SortableForeignKey' % self)
 
     def save(self, *args, **kwargs):
         if not self.id:
             try:
-                self.order = self.__class__.objects.aggregate(models.Max('order'))['order__max'] + 1
+                self.order = self.__class__.objects.aggregate(
+                    models.Max('order'))['order__max'] + 1
             except (TypeError, IndexError):
                 pass
 
